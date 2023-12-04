2a64529803a7b5d9ab35adb8bd331e0fae95f6d6
LENOVO <a.maghraby96@yahoo.com>
2023-12-04 21:10:32 +0200

    seeder

--- src/infrastructure/presistance/seeders/initial.seeder.ts
+++ src/infrastructure/presistance/seeders/initial.seeder.ts
@@ -3,7 +3,7 @@ import { SpecializationSeeder } from "./specilization.seeder";
 import { UsersSeeder } from "./users.seeder";
 
 export const DB_SEEDERS = [
-  UsersSeeder,
-  AddressSeeder,
+  // UsersSeeder,
+  // AddressSeeder,
   SpecializationSeeder
 ];
