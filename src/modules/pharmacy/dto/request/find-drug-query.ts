d213b7d08ad7ab538aefc9f0695716e047e6ce86
LENOVO <a.maghraby96@yahoo.com>
2024-01-27 16:11:22 +0200

    category id

--- src/modules/pharmacy/dto/request/find-drug-query.ts
+++ src/modules/pharmacy/dto/request/find-drug-query.ts
@@ -1,20 +1,13 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString, MIN, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MIN, MinLength } from 'class-validator';
 
 export class FindDrugQuery {
+  @ApiProperty({ required: false })
+  category_id: string;
 
-@ApiProperty ()
-@IsString()
-@IsNotEmpty()
-category_id:string
-
-@ApiProperty ()
-@IsString()
-// @MinLength(3)
-@IsNotEmpty()
-name:string
-
-
-
-
+  @ApiProperty()
+  @IsString()
+  // @MinLength(3)
+  @IsNotEmpty()
+  name: string;
 }
