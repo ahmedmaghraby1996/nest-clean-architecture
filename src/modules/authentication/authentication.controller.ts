3524763b2948f54f59639ad992cb8ee513f29d19
LENOVO <a.maghraby96@yahoo.com>
2024-02-05 18:47:22 +0200

    auth

--- src/modules/authentication/authentication.controller.ts
+++ src/modules/authentication/authentication.controller.ts
@@ -129,7 +129,7 @@ export class AuthenticationController {
   @Post(Router.Auth.Register + '/nurse')
   async registerNurse(
     @Body() req: CreateNurseRequest,
-    @UploadedFiles(new UploadValidator().build())
+    @UploadedFiles()
     files: {
       avatarFile?: Express.Multer.File[];
       license_img?: Express.Multer.File[];
